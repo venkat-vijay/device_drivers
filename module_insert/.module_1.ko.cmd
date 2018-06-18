9c9a12b324a0d4b11adc7eb65d7479a42992f454
venkat vijay <venkat_vijay23@yahoo.com>
2018-06-18 14:10:46 +0530

    module insert make file changed

---
+++ module_insert/.module_1.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/vijay/Desktop/device_module/module_insert/module_1.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/vijay/Desktop/device_module/module_insert/module_1.ko /home/vijay/Desktop/device_module/module_insert/module_1.o /home/vijay/Desktop/device_module/module_insert/module_1.mod.o ;  true
---
+++ module_insert/.tmp_versions/module_1.mod
@@ -0,0 +1,3 @@
+/home/vijay/Desktop/device_module/module_insert/module_1.ko
+/home/vijay/Desktop/device_module/module_insert/module_1.o
+
